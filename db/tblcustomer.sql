a9fb29a3c0975db71082a09c4b038555ff34d3f7
sila <sansila2222@gmail.com>
2019-04-30 17:51:50 +0700

    any update and table too

--- db/tblcustomer.sql
+++ db/tblcustomer.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Apr 30, 2019 at 02:30 AM
--- Server version: 5.5.61-38.13-log
--- PHP Version: 7.2.7
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 30, 2019 at 10:51 AM
+-- Server version: 5.7.21
+-- PHP Version: 7.0.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `estatga3_eastate`
+-- Database: `cms_admin_db`
 --
 
 -- --------------------------------------------------------
@@ -28,44 +28,31 @@ SET time_zone = "+00:00";
 -- Table structure for table `tblcustomer`
 --
 
-CREATE TABLE `tblcustomer` (
-  `customerid` int(10) NOT NULL,
+DROP TABLE IF EXISTS `tblcustomer`;
+CREATE TABLE IF NOT EXISTS `tblcustomer` (
+  `customerid` int(10) NOT NULL AUTO_INCREMENT,
   `locationid` varchar(150) DEFAULT NULL,
-  `customer_type` varchar(150) DEFAULT NULL,
+  `byroleid` int(10) DEFAULT NULL,
+  `group_name` int(10) DEFAULT NULL,
+  `company` varchar(145) DEFAULT NULL,
+  `title` varchar(145) DEFAULT NULL,
   `customer_name` varchar(145) DEFAULT NULL,
   `phone` varchar(145) DEFAULT NULL,
   `email` varchar(145) DEFAULT NULL,
   `address` varchar(245) DEFAULT NULL,
   `description` longtext,
-  `create_date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `remark` longtext,
+  `is_active` int(10) DEFAULT NULL,
+  `create_date` date DEFAULT NULL,
+  PRIMARY KEY (`customerid`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tblcustomer`
 --
 
-INSERT INTO `tblcustomer` (`customerid`, `locationid`, `customer_type`, `customer_name`, `phone`, `email`, `address`, `description`, `create_date`) VALUES
-(11, '40,42', '1', 'sila san', '0961234567', 'asasa@gmail.com', 'phnom\npehn', 'hello', '2019-04-29');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tblcustomer`
---
-ALTER TABLE `tblcustomer`
-  ADD PRIMARY KEY (`customerid`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tblcustomer`
---
-ALTER TABLE `tblcustomer`
-  MODIFY `customerid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+INSERT INTO `tblcustomer` (`customerid`, `locationid`, `byroleid`, `group_name`, `company`, `title`, `customer_name`, `phone`, `email`, `address`, `description`, `remark`, `is_active`, `create_date`) VALUES
+(11, '40,42', NULL, 1, NULL, NULL, 'sila san', '0961234567', 'asasa@gmail.com', 'phnom\npehn', 'hello', NULL, 1, '2019-04-29');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db/tblgroupcustomer.sql
+++ db/tblgroupcustomer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 30, 2019 at 06:44 AM
+-- Generation Time: Apr 30, 2019 at 10:50 AM
 -- Server version: 5.7.21
 -- PHP Version: 7.0.29
 
@@ -31,20 +31,21 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tblgroupcustomer`;
 CREATE TABLE IF NOT EXISTS `tblgroupcustomer` (
   `groupid` int(10) NOT NULL AUTO_INCREMENT,
-  `byrolerid` int(10) DEFAULT NULL,
+  `byroleid` int(10) DEFAULT NULL,
   `groupname` varchar(245) DEFAULT NULL,
   `groupnamekh` varchar(245) DEFAULT NULL,
   `is_active` int(1) DEFAULT NULL,
   `date_create` date DEFAULT NULL,
   PRIMARY KEY (`groupid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tblgroupcustomer`
 --
 
-INSERT INTO `tblgroupcustomer` (`groupid`, `byrolerid`, `groupname`, `groupnamekh`, `is_active`, `date_create`) VALUES
-(1, NULL, 'GroupA', NULL, 1, '2019-04-30');
+INSERT INTO `tblgroupcustomer` (`groupid`, `byroleid`, `groupname`, `groupnamekh`, `is_active`, `date_create`) VALUES
+(1, 1, 'GroupA', NULL, 1, '2019-04-30'),
+(2, 1, 'GroupB', NULL, 1, '2019-04-30');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
